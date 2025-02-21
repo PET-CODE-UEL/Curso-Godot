758f8f3860534f5940b38854e334b3f84bca55e3
Murilo <lilo.marino@hotmail.com>
2025-02-21 20:49:13 -0300

    Adicionado mais Câmeras

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -9,7 +9,20 @@ const JUMP_VELOCITY = 4.5
 var rotation_horizontal := 0.0
 var rotation_vertical := 0.0
 
+# Câmeras
+enum CameraModes {
+	FIRST_PERSON,
+	SECOND_PERSON,
+	THIRD_PERSON,
+}
+
+var current_camera_mode: CameraModes = CameraModes.FIRST_PERSON
+
 @onready var camera_pivot: Node3D = $CameraPivot
+@onready var first_person: Camera3D = $CameraPivot/FirstPerson
+@onready var second_person: Camera3D = $CameraPivot/SecondPerson
+@onready var third_person: Camera3D = $CameraPivot/ThirdPerson
+
 
 @onready var skeleton: Skeleton3D = $"CollisionShape/PlayerModel/Armação/Skeleton3D"
 
@@ -25,6 +38,9 @@ func _input(event):
 		rotation_horizontal -= event.relative.x * mouse_sensitivity.x
 		rotation_vertical -= event.relative.y * mouse_sensitivity.y
 		rotation_vertical = clamp(rotation_vertical, deg_to_rad(-90), deg_to_rad(90))
+	
+	if event.is_action_pressed("camera_switch"):
+		cycle_camera_mode()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -48,9 +64,16 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+func cycle_camera_mode():
+	var camera_modes := CameraModes.values()
+	current_camera_mode = camera_modes[(int(current_camera_mode) + 1) % camera_modes.size()]
+	update_camera_mode()
 
 func update_camera_mode():
-	toggle_visibility(true)
+	first_person.current = (current_camera_mode == CameraModes.FIRST_PERSON)
+	second_person.current = (current_camera_mode == CameraModes.SECOND_PERSON)
+	third_person.current = (current_camera_mode == CameraModes.THIRD_PERSON)
+	toggle_visibility(current_camera_mode == CameraModes.FIRST_PERSON)
 
 func toggle_visibility(is_first_person: bool):
 	for child in skeleton.get_children():
