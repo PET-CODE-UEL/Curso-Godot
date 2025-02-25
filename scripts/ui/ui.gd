23175eb70a8e74d96fa424344b685782b6d5d10a
Murilo <lilo.marino@hotmail.com>
2025-02-25 00:11:59 -0300

    Criado Menu de Pause

--- scripts/ui/ui.gd
+++ scripts/ui/ui.gd
@@ -1,6 +1,11 @@
 class_name UI
 extends CanvasLayer
 
+@onready var pause_menu: Control = $PauseMenu
 
 func _ready() -> void:
     UIManager.initialize(self)
+
+func _input(event):
+    if event.is_action_pressed("ui_cancel"):
+        UIManager.toggle_pause()
--- scripts/ui/ui_manager.gd
+++ scripts/ui/ui_manager.gd
@@ -1,7 +1,25 @@
 extends Node
 
+# Referência para o nó UI que contém todos os nós de interface
 var ui: UI
+
+var is_pause_open: bool = false
 
 func initialize(ui_node: CanvasLayer):
 	ui = ui_node
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func toggle_pause() -> void:
+	is_pause_open = !is_pause_open
+	ui.pause_menu.visible = is_pause_open
+	get_tree().paused = is_pause_open
+	if is_pause_open:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func force_unpause() -> void:
+	is_pause_open = false
+	ui.pause_menu.visible = false
+	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
